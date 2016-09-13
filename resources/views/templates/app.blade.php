d3be5787261bffdd8b708d2533a1b0ec66649a41
npm-to-cdn-bot (by Forbes Lindesay) <npmcdn-to-unpkg-bot@users.noreply.github.com>
2016-09-13 22:30:39 +0100

    Replace npmcdn.com with unpkg.com

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -35,8 +35,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
-<script src="https://npmcdn.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
-<script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
+<script src="https://unpkg.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 <script src="{{ url('js/vue.min.js') }}"></script>
 <script src="{{ url('js/vue-resource.min.js') }}"></script>
 <script>
